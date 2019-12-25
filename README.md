546027c78453f442dabceeea8fa679af236c0aac
Oleg Kravchenko <okravc@gmail.com>
2019-12-25 14:03:54 +0300

    run citest:ccm-pg daemons

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# npm golang psql cqlsh gcloud kubectl docker-cli
+# citest - npm golang psql cqlsh gcloud kubectl docker-cli
+# citest:ccm-pg - added cassandra ccm and postgresql server
 
 ```
 docker run -ti citest
+
+docker run -e PG_USER=test -e PG_PASS=test -e PG_AUTH=md5 -e PG_DBNAME=test citest:ccm-pg
+
+docker exec -ti $(docker ps -lq) bash
 ```
--- build-ccm-pg.sh
+++ build-ccm-pg.sh
@@ -4,9 +4,9 @@ set -e
 
 docker build \
        $@ \
-       -t citest-ccm-pg . \
+       -t citest:ccm-pg . \
        -f Dockerfile-ccm-pg \
        --build-arg=SOURCE_COMMIT=$(git rev-parse --short HEAD) \
        --build-arg=SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-docker tag citest-ccm-pg porzione/citest-ccm-pg
+docker tag citest:ccm-pg porzione/citest:ccm-pg
